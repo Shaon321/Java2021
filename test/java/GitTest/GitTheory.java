ba496e5d68576a59ed52d973d65700aab597a46b
Shaon321 <lumaknun@gmail.com>
2022-01-17 14:09:21 -0800

    2nd commit

---
+++ test/java/GitTest/GitTheory.java
@@ -0,0 +1,301 @@
+package GitTest;
+
+public class GitTheory {
+	/*
+	 * Microsoft Windows [Version 10.0.19042.1466]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\lmakn>pwd
+'pwd' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\lmakn>dir
+ Volume in drive C is Windows-SSD
+ Volume Serial Number is 485F-2D2D
+
+ Directory of C:\Users\lmakn
+
+11/12/2021  11:05 AM    <DIR>          .
+11/12/2021  11:05 AM    <DIR>          ..
+09/27/2021  04:39 PM    <DIR>          .android
+10/15/2021  10:34 AM    <DIR>          .cache
+11/12/2021  11:05 AM    <DIR>          .config
+07/06/2021  09:35 AM    <DIR>          .eclipse
+09/23/2021  08:55 PM                52 .gitconfig
+11/11/2021  10:59 PM    <DIR>          .groovy
+11/15/2021  12:23 PM    <DIR>          .jenkins
+09/16/2021  04:08 PM    <DIR>          .lemminx
+07/15/2021  05:28 PM    <DIR>          .m2
+01/17/2022  11:46 AM    <DIR>          .p2
+11/03/2021  01:20 PM    <DIR>          .vscode
+06/22/2021  03:54 AM    <DIR>          3D Objects
+08/19/2021  10:42 AM               151 BullseyeCoverageError.txt
+06/22/2021  03:54 AM    <DIR>          Contacts
+09/23/2021  08:34 PM    <DIR>          CucumberBDD_ClassWork
+01/12/2022  09:02 PM    <DIR>          Desktop
+01/16/2022  10:40 AM    <DIR>          Documents
+01/17/2022  11:31 AM    <DIR>          Downloads
+06/21/2021  03:13 PM    <DIR>          eclipse
+12/27/2021  12:42 PM    <DIR>          eclipse-workspace
+06/22/2021  03:54 AM    <DIR>          Favorites
+06/22/2021  03:54 AM    <DIR>          Links
+06/22/2021  03:54 AM    <DIR>          Music
+10/28/2021  01:04 PM                 0 mvn
+01/17/2022  11:01 AM    <DIR>          OneDrive
+12/24/2021  10:56 AM    <DIR>          Pictures
+08/29/2021  07:24 AM    <DIR>          Postman
+06/22/2021  03:54 AM    <DIR>          Saved Games
+06/22/2021  03:55 AM    <DIR>          Searches
+06/21/2021  04:06 PM    <DIR>          Videos
+               3 File(s)            203 bytes
+              29 Dir(s)  225,754,075,136 bytes free
+
+C:\Users\lmakn>cd eclipse-workspace
+
+C:\Users\lmakn\eclipse-workspace>dir
+ Volume in drive C is Windows-SSD
+ Volume Serial Number is 485F-2D2D
+
+ Directory of C:\Users\lmakn\eclipse-workspace
+
+12/27/2021  12:42 PM    <DIR>          .
+12/27/2021  12:42 PM    <DIR>          ..
+12/31/2021  08:24 AM    <DIR>          .metadata
+12/31/2021  07:54 AM    <DIR>          com.selenium2022
+08/13/2021  06:29 PM    <DIR>          DraftSel2021
+06/24/2021  10:10 AM    <DIR>          JavaHomeWork
+08/11/2021  09:30 AM    <DIR>          OnlineJune2021
+09/29/2021  10:22 AM    <DIR>          OnlineSelJune2021
+08/11/2021  10:03 AM    <DIR>          SelHWJune2021
+               0 File(s)              0 bytes
+               9 Dir(s)  225,751,773,184 bytes free
+
+C:\Users\lmakn\eclipse-workspace>cd com.selenium2022
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022>dir
+ Volume in drive C is Windows-SSD
+ Volume Serial Number is 485F-2D2D
+
+ Directory of C:\Users\lmakn\eclipse-workspace\com.selenium2022
+
+12/31/2021  07:54 AM    <DIR>          .
+12/31/2021  07:54 AM    <DIR>          ..
+12/31/2021  08:10 AM             1,132 .classpath
+12/27/2021  12:42 PM               568 .project
+12/27/2021  12:42 PM    <DIR>          .settings
+12/31/2021  07:56 AM    <DIR>          driver
+12/27/2021  12:50 PM             3,289 pom.xml
+12/27/2021  12:42 PM    <DIR>          src
+12/27/2021  12:42 PM    <DIR>          target
+01/06/2022  01:00 PM    <DIR>          test-output
+               3 File(s)          4,989 bytes
+               7 Dir(s)  225,749,356,544 bytes free
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022>cd src
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>dir
+ Volume in drive C is Windows-SSD
+ Volume Serial Number is 485F-2D2D
+
+ Directory of C:\Users\lmakn\eclipse-workspace\com.selenium2022\src
+
+12/27/2021  12:42 PM    <DIR>          .
+12/27/2021  12:42 PM    <DIR>          ..
+12/27/2021  12:42 PM    <DIR>          main
+12/27/2021  12:42 PM    <DIR>          test
+               0 File(s)              0 bytes
+               4 Dir(s)  225,746,223,104 bytes free
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git init
+Initialized empty Git repository in C:/Users/lmakn/eclipse-workspace/com.selenium2022/src/.git/
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git add .
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git status
+On branch master
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   test/java/Abstraction1/Animal.java
+        new file:   test/java/Abstraction1/Animal2.java
+        new file:   test/java/Abstraction1/Animal3.java
+        new file:   test/java/Abstraction1/MainClass.java
+        new file:   test/java/Abstraction1/Pig.java
+        new file:   test/java/Abstraction1/Pig2.java
+        new file:   test/java/Abstraction1/Pig3.java
+        new file:   test/java/Date1/CurrentDate.java
+        new file:   test/java/Date1/CurrentDateTime.java
+        new file:   test/java/Date1/DateTheory.java
+        new file:   test/java/Date1/DateTimeFormatting.java
+        new file:   test/java/Date1/LocalTime.java
+        new file:   test/java/Enum1/EnumMain.java
+        new file:   test/java/Enum1/EnumTheory.java
+        new file:   test/java/Inheritance1/InheritCar.java
+        new file:   test/java/Inheritance1/InheritFinalVehicle.java
+        new file:   test/java/Inheritance1/InheritFinalVehicle2.java
+        new file:   test/java/Inheritance1/InheritVehicle.java
+        new file:   test/java/Interface1/Animal2Int.java
+        new file:   test/java/Interface1/Animal3Int.java
+        new file:   test/java/Interface1/AnimalInt.java
+        new file:   test/java/Interface1/Democlass.java
+        new file:   test/java/Interface1/FirstInterface.java
+        new file:   test/java/Interface1/MainClass.java
+        new file:   test/java/Interface1/MainClassMultipleInt.java
+        new file:   test/java/Interface1/Pig2Reg.java
+        new file:   test/java/Interface1/Pig3Reg.java
+        new file:   test/java/Interface1/PigReg.java
+        new file:   test/java/Interface1/SecondInterface.java
+        new file:   test/java/Methods1/Method2.java
+        new file:   test/java/Methods1/MethodOverloading.java
+        new file:   test/java/Methods1/MethodsTheory.java
+        new file:   test/java/Methods1/ReturnMethods.java
+        new file:   test/java/Polymorphism1/Animal.java
+        new file:   test/java/Polymorphism1/CallClass.java
+        new file:   test/java/Polymorphism1/Dog.java
+        new file:   test/java/Polymorphism1/Pig.java
+        new file:   test/java/Scanner1/ScannerMain.java
+        new file:   test/java/Scanner1/ScannerTheory.java
+        new file:   test/java/com/selenium2022/AlluserDAtaExcel.java
+        new file:   test/java/com/selenium2022/ArrayList1.java
+        new file:   test/java/com/selenium2022/ArrayList2.java
+        new file:   test/java/com/selenium2022/ArrayList3.java
+        new file:   test/java/com/selenium2022/ArrayList4.java
+        new file:   test/java/com/selenium2022/ArrayMutiDimensional.java
+        new file:   test/java/com/selenium2022/Arrays1.java
+        new file:   test/java/com/selenium2022/DateTime.java
+        new file:   test/java/com/selenium2022/DragDropTTK.java
+        new file:   test/java/com/selenium2022/ExcelMortCalc.java
+        new file:   test/java/com/selenium2022/HashMap1.java
+        new file:   test/java/com/selenium2022/HashMap2.java
+        new file:   test/java/com/selenium2022/HashSet1.java
+        new file:   test/java/com/selenium2022/Iterator1.java
+        new file:   test/java/com/selenium2022/Iterator2.java
+        new file:   test/java/com/selenium2022/LinkedList1.java
+        new file:   test/java/com/selenium2022/MortCal.java
+        new file:   test/java/com/selenium2022/MouseHoverOverTTK.java
+        new file:   test/java/com/selenium2022/ScrollByVisibleElements.java
+        new file:   test/java/com/selenium2022/SliderRange.java
+        new file:   test/java/com/selenium2022/SliderRangeTTK.java
+        new file:   test/java/com/selenium2022/StaticVarriable1.java
+        new file:   test/java/com/selenium2022/ThrowException.java
+        new file:   test/java/com/selenium2022/TryCatch1.java
+        new file:   test/java/com/selenium2022/WrapperClass1.java
+        new file:   test/java/oopConcept/AbstractTheory.java
+        new file:   test/java/oopConcept/Animal.java
+        new file:   test/java/oopConcept/ConstructorTheory.java
+        new file:   test/java/oopConcept/Encapsulation1.java
+        new file:   test/java/oopConcept/EncapsulationTheory.java
+        new file:   test/java/oopConcept/InheritanceTheory.java
+        new file:   test/java/oopConcept/InnerTheory.java
+        new file:   test/java/oopConcept/InterfaceTheory.java
+        new file:   test/java/oopConcept/ModifierTheory.java
+        new file:   test/java/oopConcept/OopTheory.java
+        new file:   test/java/oopConcept/PolymorphismTheory.java
+
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git commit -m "First commit"
+[master (root-commit) 329ba69] First commit
+ 75 files changed, 1973 insertions(+)
+ create mode 100644 test/java/Abstraction1/Animal.java
+ create mode 100644 test/java/Abstraction1/Animal2.java
+ create mode 100644 test/java/Abstraction1/Animal3.java
+ create mode 100644 test/java/Abstraction1/MainClass.java
+ create mode 100644 test/java/Abstraction1/Pig.java
+ create mode 100644 test/java/Abstraction1/Pig2.java
+ create mode 100644 test/java/Abstraction1/Pig3.java
+ create mode 100644 test/java/Date1/CurrentDate.java
+ create mode 100644 test/java/Date1/CurrentDateTime.java
+ create mode 100644 test/java/Date1/DateTheory.java
+ create mode 100644 test/java/Date1/DateTimeFormatting.java
+ create mode 100644 test/java/Date1/LocalTime.java
+ create mode 100644 test/java/Enum1/EnumMain.java
+ create mode 100644 test/java/Enum1/EnumTheory.java
+ create mode 100644 test/java/Inheritance1/InheritCar.java
+ create mode 100644 test/java/Inheritance1/InheritFinalVehicle.java
+ create mode 100644 test/java/Inheritance1/InheritFinalVehicle2.java
+ create mode 100644 test/java/Inheritance1/InheritVehicle.java
+ create mode 100644 test/java/Interface1/Animal2Int.java
+ create mode 100644 test/java/Interface1/Animal3Int.java
+ create mode 100644 test/java/Interface1/AnimalInt.java
+ create mode 100644 test/java/Interface1/Democlass.java
+ create mode 100644 test/java/Interface1/FirstInterface.java
+ create mode 100644 test/java/Interface1/MainClass.java
+ create mode 100644 test/java/Interface1/MainClassMultipleInt.java
+ create mode 100644 test/java/Interface1/Pig2Reg.java
+ create mode 100644 test/java/Interface1/Pig3Reg.java
+ create mode 100644 test/java/Interface1/PigReg.java
+ create mode 100644 test/java/Interface1/SecondInterface.java
+ create mode 100644 test/java/Methods1/Method2.java
+ create mode 100644 test/java/Methods1/MethodOverloading.java
+ create mode 100644 test/java/Methods1/MethodsTheory.java
+ create mode 100644 test/java/Methods1/ReturnMethods.java
+ create mode 100644 test/java/Polymorphism1/Animal.java
+ create mode 100644 test/java/Polymorphism1/CallClass.java
+ create mode 100644 test/java/Polymorphism1/Dog.java
+ create mode 100644 test/java/Polymorphism1/Pig.java
+ create mode 100644 test/java/Scanner1/ScannerMain.java
+ create mode 100644 test/java/Scanner1/ScannerTheory.java
+ create mode 100644 test/java/com/selenium2022/AlluserDAtaExcel.java
+ create mode 100644 test/java/com/selenium2022/ArrayList1.java
+ create mode 100644 test/java/com/selenium2022/ArrayList2.java
+ create mode 100644 test/java/com/selenium2022/ArrayList3.java
+ create mode 100644 test/java/com/selenium2022/ArrayList4.java
+ create mode 100644 test/java/com/selenium2022/ArrayMutiDimensional.java
+ create mode 100644 test/java/com/selenium2022/Arrays1.java
+ create mode 100644 test/java/com/selenium2022/DateTime.java
+ create mode 100644 test/java/com/selenium2022/DragDropTTK.java
+ create mode 100644 test/java/com/selenium2022/ExcelMortCalc.java
+ create mode 100644 test/java/com/selenium2022/HashMap1.java
+ create mode 100644 test/java/com/selenium2022/HashMap2.java
+ create mode 100644 test/java/com/selenium2022/HashSet1.java
+ create mode 100644 test/java/com/selenium2022/Iterator1.java
+ create mode 100644 test/java/com/selenium2022/Iterator2.java
+ create mode 100644 test/java/com/selenium2022/LinkedList1.java
+ create mode 100644 test/java/com/selenium2022/MortCal.java
+ create mode 100644 test/java/com/selenium2022/MouseHoverOverTTK.java
+ create mode 100644 test/java/com/selenium2022/ScrollByVisibleElements.java
+ create mode 100644 test/java/com/selenium2022/SliderRange.java
+ create mode 100644 test/java/com/selenium2022/SliderRangeTTK.java
+ create mode 100644 test/java/com/selenium2022/StaticVarriable1.java
+ create mode 100644 test/java/com/selenium2022/ThrowException.java
+ create mode 100644 test/java/com/selenium2022/TryCatch1.java
+ create mode 100644 test/java/com/selenium2022/WrapperClass1.java
+ create mode 100644 test/java/oopConcept/AbstractTheory.java
+ create mode 100644 test/java/oopConcept/Animal.java
+ create mode 100644 test/java/oopConcept/ConstructorTheory.java
+ create mode 100644 test/java/oopConcept/Encapsulation1.java
+ create mode 100644 test/java/oopConcept/EncapsulationTheory.java
+ create mode 100644 test/java/oopConcept/InheritanceTheory.java
+ create mode 100644 test/java/oopConcept/InnerTheory.java
+ create mode 100644 test/java/oopConcept/InterfaceTheory.java
+ create mode 100644 test/java/oopConcept/ModifierTheory.java
+ create mode 100644 test/java/oopConcept/OopTheory.java
+ create mode 100644 test/java/oopConcept/PolymorphismTheory.java
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git status
+On branch master
+nothing to commit, working tree clean
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git remote add origin https://github.com/Shaon321/Java2021.git
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>git push origin master
+Enumerating objects: 90, done.
+Counting objects: 100% (90/90), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (87/87), done.
+Writing objects: 100% (90/90), 27.17 KiB | 1.23 MiB/s, done.
+Total 90 (delta 5), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (5/5), done.
+remote:
+remote: Create a pull request for 'master' on GitHub by visiting:
+remote:      https://github.com/Shaon321/Java2021/pull/new/master
+remote:
+To https://github.com/Shaon321/Java2021.git
+ * [new branch]      master -> master
+
+C:\Users\lmakn\eclipse-workspace\com.selenium2022\src>
+	 */
+
+}
